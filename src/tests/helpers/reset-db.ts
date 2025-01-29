c9d784e3d86a6193d784774b937b68230e8fb22e
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2025-01-30 00:39:00 +0530

    reset function for cleaning db before running a new test (describe block)

---
+++ src/tests/helpers/reset-db.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const resetDb = async () => {
+  await prisma.$transaction([prisma.request.deleteMany()]);
+};
